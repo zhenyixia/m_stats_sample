2cab3cf489cc3f2e67f72a405a2a9e4fa04c2df7
zhenyixia <zyx_2017@163.com>
2021-12-31 19:48:09 +0800

    集成前台代码，可以打包成jar包，准备生成exe

--- src/main/java/com/lyp/count/common/config/OpenBrowser.java
+++ src/main/java/com/lyp/count/common/config/OpenBrowser.java
@@ -12,7 +12,7 @@ public class OpenBrowser implements CommandLineRunner{
   public void run(String... args) throws Exception{
     log.info("应用已经准备就绪 ... 启动浏览器并自动加载指定的页面 ... ");
     try {
-      Runtime.getRuntime().exec("cmd /c start http://localhost:8081/index.html#/countHome");//指定自己项目的路径
+      Runtime.getRuntime().exec("cmd /c start http://localhost:8090/index.html/#/overview");//指定自己项目的路径
     } catch (Exception ex) {
       ex.printStackTrace();
     }
